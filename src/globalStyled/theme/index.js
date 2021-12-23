1b6ddb7ec271bd4de342dd287548d0dac348483f
Austin <austin@aidmics.com>
2021-12-23 16:28:07 +0800

    [chore] add global theme for styled components

---
+++ src/globalStyled/theme/index.js
@@ -0,0 +1,21 @@
+const theme = {
+  color: {
+    primary: '#001b59',
+    dark: '#222222',
+    light_dark: '#444444',
+    gray: '#cccccc',
+    light_gray: '#eaeaea',
+    active: '#ffcb70',
+  },
+  rect: {
+    header_height: '90px',
+    nav_width: '230px',
+    nav_collapsed_width: '80px',
+    nav_logo_height: '60px',
+  },
+  padding: {
+    content_padding: '1rem',
+  }
+}
+
+export default theme
