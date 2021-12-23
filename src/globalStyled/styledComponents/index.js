70b2f00693da13df2ac0022db123064d0b3617f7
Austin <austin@aidmics.com>
2021-12-23 16:27:48 +0800

    [chore] add global styled components

---
+++ src/globalStyled/styledComponents/index.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+
+const MainContent = styled.div`
+  padding: 1.2rem 1rem;
+`
+
+const FontIcon = styled.i`
+  color: ${props => {
+    const {
+      theme,
+      color,
+    } = props
+    return color || theme.color.light_gray
+  }};
+  font-size: ${props => {
+    const {
+      size,
+    } = props
+    return size ? `${size}px` : 'inherit'
+  }};
+
+  & + * {
+    margin-left: 5px;
+  }
+`
+
+export {
+  MainContent,
+  FontIcon,
+}
