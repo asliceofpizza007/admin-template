e137f2534cdab367c9f31f4720b4da716c3ad8e5
Austin <austin@aidmics.com>
2021-12-23 16:32:42 +0800

    [chore] add nav bar with reactive menus

---
+++ src/containers/AuthLayout/subComponents/Navbar/Menus/index.js
@@ -0,0 +1,224 @@
+import {
+  useRef,
+  useMemo,
+  useCallback,
+  useState,
+  useLayoutEffect,
+} from 'react'
+import { Menu } from 'antd'
+import { useTranslation } from 'react-i18next'
+import {
+  matchRoutes,
+  useLocation,
+  useNavigate,
+} from 'react-router-dom'
+import { isArray } from "lodash";
+import {
+  useDispatch,
+  useSelector,
+} from "react-redux";
+import { toggleNav } from '@redux/modules/layouts'
+import { camelToUnderscore } from '@utils';
+
+const {
+  Item,
+  SubMenu,
+} = Menu
+
+const Menus = () => {
+  const { t } = useTranslation('nav')
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const location = useLocation()
+  const isNavCollapsed = useSelector(state => state.layouts.isNavCollapsed)
+  const isMobile = useSelector(state => state.layouts.isMobile)
+  const role = useSelector(state => state.account.role)
+
+  const userNavigation = useRef([
+    {
+      name: 'user',
+      icon: (
+        <i className="aidmics-user" />
+      ),
+      menus: [
+        {
+          name: 'UserList',
+          icon: (
+            <i className="aidmics-users" />
+          ),
+          path: '/users',
+          deps_routes: [
+            '/trends/:animal_id',
+            '/trends',
+          ]
+        }
+      ]
+    }
+  ])
+  const serialNavigation = useRef([
+    {
+      name: 'serial',
+      icon: (
+        <i className="aidmics-list" />
+      ),
+      menus: [
+        {
+          name: 'SerialList',
+          icon: (
+            <i className="aidmics-list" />
+          ),
+          path: '/serials',
+          deps_routes: [
+            '/serials/:serial_id',
+          ]
+        },
+        {
+          name: 'OrderList',
+          icon: (
+            <i className="aidmics-layer" />
+          ),
+          path: '/orders',
+        },
+      ]
+    }
+  ])
+  const mergedNavigation = useMemo(() => {
+    const navigation = [...serialNavigation.current]
+    if(['Admin', 'Staff'].includes(role)) {
+      navigation.splice(0, 0, ...userNavigation.current)
+    }
+    return navigation
+  }, [role])
+
+  const generateNav = useCallback((list = []) => {
+    if(!isArray(list)) return
+    const nav = list.map(item => {
+      const {
+        name,
+        icon,
+        path,
+        menus,
+      } = item
+      const title = t(camelToUnderscore(name))
+      if(menus?.length) {
+        return (
+          <SubMenu
+            key={name}
+            title={title}
+            icon={icon}
+          >
+            { generateNav(menus) }
+          </SubMenu>
+        )
+      }
+      return (
+        <Item
+          key={path}
+          icon={icon}
+        >
+          { title }
+        </Item>
+      )
+    })
+    return nav
+  }, [])
+
+  const items = useMemo(() => {
+    return generateNav(mergedNavigation)
+  }, [t, mergedNavigation])
+
+  const [selectedKeys, setSelectedKeys] = useState()
+  const [openKeys, setOpenKeys] = useState()
+  const createHash = useCallback((list = [], hash = {}, keys = []) => {
+    if(!isArray(list)) return
+    list.forEach(item => {
+      const openKeyPath = [
+        ...keys,
+      ]
+      const {
+        name,
+        menus,
+        path,
+        deps_routes,
+      } = item
+      if(menus?.length) {
+        openKeyPath.push(name)
+        createHash(menus, hash, openKeyPath)
+      } else {
+        let routes = [path]
+        if(deps_routes?.length) {
+          routes = routes.concat(deps_routes)
+        }
+        const routeObjects = routes.map(route => {
+          return {
+            path: `/auth${route}`,
+          }
+        })
+        hash[path] = {
+          openKeyPath,
+          routeObjects,
+        }
+      }
+    })
+    return hash
+  }, [])
+  const hash = useMemo(() => {
+    return createHash(mergedNavigation)
+  }, [mergedNavigation])
+  useLayoutEffect(() => {
+    Object.keys(hash).forEach(key => {
+      const {
+        openKeyPath,
+        routeObjects,
+      } = hash[key]
+      const matches = matchRoutes(routeObjects, location)
+      if(matches) {
+        setOpenKeys(openKeyPath)
+        setSelectedKeys(key)
+      }
+    })
+    const match = matchRoutes([{ path: '/auth'}], location)
+    if(match) {
+      setOpenKeys([])
+      setSelectedKeys('dashboard')
+    }
+  }, [location])
+
+  const handleOnNavClick = useCallback(({key}) => {
+    const path = `/auth${key === 'dashboard' ? '' : key}`
+    if(path === location.pathname) return
+    navigate(path)
+    if(isMobile) {
+      dispatch(toggleNav())
+    }
+  }, [isMobile, location])
+
+  const handleOnOpenChange = useCallback((keys) => {
+    setOpenKeys(keys)
+  }, [])
+
+  return (
+    <Menu
+      style={{ height: '100%', }}
+      selectedKeys={selectedKeys}
+      openKeys={openKeys}
+      mode="inline"
+      theme="dark"
+      inlineCollapsed={isNavCollapsed && !isMobile}
+      onClick={handleOnNavClick}
+      onOpenChange={handleOnOpenChange}
+    >
+      <Item
+        key="dashboard"
+        icon={
+          <i className="aidmics-home" />
+        }
+      >
+        { t('dashboard') }
+      </Item>
+      { items }
+    </Menu>
+  )
+}
+
+export default Menus
---
+++ src/containers/AuthLayout/subComponents/Navbar/index.js
@@ -0,0 +1,103 @@
+import styled from "styled-components";
+import {
+  useDispatch,
+  useSelector,
+} from "react-redux";
+import { toggleNav } from '@redux/modules/layouts'
+import { Scrollbar } from '@components'
+import logo from '@images/logo.png'
+import Menus from './Menus'
+
+
+const Nav = styled.nav`
+  position: fixed;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  width: ${props => {
+    const {
+      theme,
+      collapsed,
+    } = props
+    const { rect } = theme
+    return collapsed ?
+      rect.nav_collapsed_width :
+      rect.nav_width
+  }};
+  background-color: #001529;
+  transition: width 0.2s ease-in-out, left 0.2s ease-in-out;
+  overflow: hidden;
+  z-index: 10;
+  @media (max-width: 996px) {
+    width: ${props => props.theme.rect.nav_width};
+    left: ${props => props.collapsed ? '-230px' : '0'};
+  }
+`
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  width: 100%;
+  height: ${props => props.theme.rect.nav_logo_height};
+  padding: 0 24px;
+  color: rgba(255, 255, 255, 0.85);
+  font-size: 24px;
+  > img {
+    width: 40px;
+    margin: unset;
+  }
+  p {
+    margin-left: 10px;
+    white-space: nowrap;
+    font-weight: bold;
+  }
+`
+const Mask = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.6);
+  z-index: 9;
+`
+
+const Navbar = (props) => {
+  const dispatch = useDispatch()
+  const isNavCollapsed = useSelector(state => state.layouts.isNavCollapsed)
+  const isMobile = useSelector(state => state.layouts.isMobile)
+
+
+  return (
+    <>
+      <Nav collapsed={isNavCollapsed}>
+        <Logo>
+          <img src={logo} alt="logo" />
+          {
+            !isNavCollapsed &&
+            <p>
+              Aidmics
+            </p>
+          }
+        </Logo>
+        <Scrollbar
+          style={{
+            height: 'calc(100% - 60px)',
+          }}
+        >
+          <Menus />
+        </Scrollbar>
+      </Nav>
+      {
+        isMobile && !isNavCollapsed &&
+        <Mask
+          onClick={() => {
+            dispatch(toggleNav())
+          }}
+        />
+      }
+    </>
+  )
+}
+
+export default Navbar
