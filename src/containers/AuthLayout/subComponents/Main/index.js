f8c142841485097ee31a570fe6c59daedf157697
Austin <austin@aidmics.com>
2021-12-23 16:32:20 +0800

    [chore] add main layout for tab view outlet

---
+++ src/containers/AuthLayout/subComponents/Main/index.js
@@ -0,0 +1,27 @@
+import { useEffect } from 'react'
+import styled from "styled-components";
+import {
+  Outlet,
+  useLocation,
+} from 'react-router-dom'
+
+const MainLayout = styled.main`
+  position: relative;
+  min-height: calc(200vh - ${props => props.theme.rect.header_height});
+`
+
+const Main = (props) => {
+  const location = useLocation()
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+    })
+  }, [location])
+  return (
+    <MainLayout>
+      <Outlet />
+    </MainLayout>
+  )
+}
+
+export default Main
