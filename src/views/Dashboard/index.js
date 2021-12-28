1768800961116bfe9f0b93b4b3276723bd0a7dec
Austin <austin@aidmics.com>
2021-12-28 18:42:47 +0800

    [chore] add demo code on dashboard page

--- src/views/Dashboard/index.js
+++ src/views/Dashboard/index.js
@@ -1,10 +1,31 @@
-import React from "react";
+import styled from 'styled-components'
+import {
+  MainContent,
+} from '@styled/styledComponents'
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`
 
 const Dashboard = () => {
+  const arr = Array(1000).fill('text')
   return (
-    <div>
-      dashboard
-    </div>
+    <MainContent>
+      <Content>
+        {
+          arr.map((el,i) => {
+            return (
+              <p key={i}>
+                {`${el} - ${i}\n`}
+                Dashboard scrollbar UI/UX demo
+              </p>
+            )
+          })
+        }
+      </Content>
+    </MainContent>
   )
 }
 
