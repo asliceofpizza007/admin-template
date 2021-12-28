709827ae614f5df09977bd0c73aa7f4b190feba7
Austin <austin@aidmics.com>
2021-12-28 18:43:24 +0800

    [feat] tabs auto scroll

--- src/components/Scrollbar/index.js
+++ src/components/Scrollbar/index.js
@@ -1,18 +1,29 @@
 import {
   useRef,
   useEffect,
+  useCallback,
 } from 'react'
 import SmoothScrollbar from 'smooth-scrollbar'
-import OverscrollPlugin from 'smooth-scrollbar/plugins/overscroll'
+import { useSizeObserve } from '@hooks'
+// import OverscrollPlugin from 'smooth-scrollbar/plugins/overscroll'
 
 const Scrollbar = (props) => {
   const {
+    anchor,
+    autoScroll = false,
+    scrollDirection = 'width', // width or height
     style,
     alwaysShowTracks = false,
-    overscroll = false,
+    // overscroll = false,
     children,
   } = props
   const el = useRef(null)
+  const instance = useRef(null)
+  const {
+    observer,
+    width,
+    height,
+  } = useSizeObserve()
   useEffect(()=> {
     if(!el.current) return
     const options = {
@@ -23,12 +34,33 @@ const Scrollbar = (props) => {
         }
       }
     }
-    let instance = SmoothScrollbar.init(el.current, options)
+    instance.current = SmoothScrollbar.init(el.current, options)
+    observer.observe(el.current)
     return () => {
-      instance.destroy()
-      instance = null // for gc
+      observer.disconnect()
+      instance.current.destroy()
+      instance.current = null // for gc
     }
   }, [])
+
+  
+
+  const handleAnchor = useCallback(e => {
+    // don't auto scroll when there's no anchor assigned or scroll bar visible
+    if(!autoScroll || !anchor) return
+    const {
+      container,
+      content,
+    } = instance.current.getSize()
+    const hasScrollbar = content[scrollDirection] > container[scrollDirection]
+    if(!hasScrollbar) return
+    const el = document.querySelector(`#${anchor}`)
+    instance.current.scrollIntoView(el)
+  }, [anchor, instance])
+
+  useEffect(() => {
+    handleAnchor()
+  }, [width, height, anchor])
 
   return (
     <div
--- src/containers/AuthLayout/subComponents/Header/NavTabs/index.js
+++ src/containers/AuthLayout/subComponents/Header/NavTabs/index.js
@@ -1,9 +1,14 @@
+import { useMemo } from "react";
 import styled from "styled-components";
 import { useAliveController } from "react-activation";
 import {
   CSSTransition,
   TransitionGroup,
 } from 'react-transition-group'
+import {
+  useLocation,
+  matchRoutes,
+} from "react-router-dom";
 import { Scrollbar } from "@components";
 import Tab from './Tab'
 
@@ -19,11 +24,33 @@ const TabsContent = styled.div`
 
 const NavTabs = (props) => {
   const { getCachingNodes } = useAliveController()
-  const tabs = getCachingNodes().filter(node => node.name !== 'Dashboard')
+  const allNodes = getCachingNodes()
+  const tabs = allNodes.filter(node => node.name !== 'Dashboard')
+
+  const location = useLocation()
+  const activeAnchor = useMemo(() => {
+    const nodes = [
+      {
+        name: 'dashboard',
+        path: '',
+      },
+      ...allNodes,
+    ]
+    return nodes.find(node => {
+      const { path } = node
+      const match = matchRoutes([{
+        path: `/auth${path}`
+      }], location)
+      return match
+    })
+  }, [allNodes, location])
 
   return (
     <TabsContainer>
-      <Scrollbar>
+      <Scrollbar
+        anchor={activeAnchor?.name}
+        autoScroll
+      >
         <TabsContent>
           <Tab
             id="dashboard"
